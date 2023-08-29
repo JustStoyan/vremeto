6d9e871fee1f411b8736ce0569f74f1992cda308
Stoyan Veselinov <s.a.veselinov@gmail.com>
2023-08-29 05:45:19 +0300

    Added a notification manager for errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import styles from "./App.module.css";
 import { DetailedView } from "./components/DetailedView";
 import { QuickView } from "./components/QuickView";
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   return (
     <div className={styles["app-wrapper"]}>
       <div className={styles.app}>
+        <ToastContainer />
         <QuickView />
         <DetailedView />
       </div>
